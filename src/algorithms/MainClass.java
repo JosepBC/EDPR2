b8d0df227c45ee8b891078a6d306689ca3f9f62d
Josep Bello <bellocontacto@gmail.com>
2020-06-10 02:05:15 +0200

    Pequeño juego de pruebas de la clase Graph

--- src/algorithms/MainClass.java
+++ src/algorithms/MainClass.java
@@ -1,10 +1,29 @@
 package algorithms;
 
+import dataStructures.Graph;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Graph <Integer, String> myGraph1 = new Graph<>();
+		Graph <Integer, String> myGraph2 = new Graph<>();
+		
+		myGraph1.addNode(0);
+		myGraph1.addNode(20);
+		
+		myGraph2.addNode(0);
+		myGraph2.addNode(20);
+		
+		myGraph1.addEdge(0, 20, "Hola", true);
+		myGraph2.addEdge(0, 20, "Hola", false);
+		
+		System.out.println(myGraph1.toString());
+		System.out.println(myGraph2.toString());
+		
+		System.out.println(myGraph1.getLinks(0));
+		
+		myGraph2.removeNode(0);
+		System.out.println(myGraph2.toString());
 	}
 
 }
